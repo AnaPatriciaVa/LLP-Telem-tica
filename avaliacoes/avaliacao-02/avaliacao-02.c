49bdd64ab92432c1974a9a04db89249b42fa8b6b
AnaPatriciaVa <111452257+AnaPatriciaVa@users.noreply.github.com>
2022-09-27 18:04:30 -0300

    Update avaliacao-02.c

--- avaliacoes/avaliacao-02/avaliacao-02.c
+++ avaliacoes/avaliacao-02/avaliacao-02.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+int main() {
+  int i;
+
+  printf("Questão 1 - a) Qual a diferença entre o while e o do .. while;  \n\n");
+  printf("R.: O Do While e  While tem quase a mesmo funcionamento , a diferença é que o Do While, com o uso dele teremos os comandos executados ao menos uma única vez.\n\n");
+  
+  printf("Questão 1 - item b:\n");
+  for ( i=0; i<=100; i++) {
+    if ((i% 5 ==0) && (i% 2 ==0)) {
+      printf("%d\n",i);
+    }  
+  }
+
+  printf("\nQuestão 2 - item a:\n");
+  i=-1;
+  while ( i<100){
+    i++;
+    if(i %4 !=0) {
+      continue;
+    } else {
+      printf( "%d\n",i);
+    }
+  }
+  return 0;
+}
