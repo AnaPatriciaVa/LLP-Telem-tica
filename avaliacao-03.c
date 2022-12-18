c33ec373bec3b08e5eb06e325d924cedbbc09c92
AnaPatriciaVa <111452257+AnaPatriciaVa@users.noreply.github.com>
2022-12-17 21:53:38 -0300

    Update avaliacao-03.c

--- avaliacao-03.c
+++ avaliacao-03.c
@@ -4,40 +4,32 @@
 // a) Dados de entrada (a, b, c inteiros que correspondem na função quadrática F(x) =
 // ax**2 + bx + c). Pede-se as raizes da função quando Delta = 0 e Delta > 0. Quando o
 // Delta for < 0 printar a mensagem "Raízes Imaginárias".
-#include <studio.h>
-int main ()
+#include <stdio.h>
+#include <math.h>
+int main()
 {
-  float a,b,c , delta, x1,x2;
-  float sqrtdelta, raiz_1,raiz_2;
-  
-  printf("f(x) =ax2 +bx+c = 0\n");
-  printf("a = 1"); scanf("%f",&a);
-  printf("b = 4"); scanf("%f" ,&b);
-  printf("c = 2"); scanf("%f",&c);
-  
-  if(a != 0 && c != 0)
-  {
-    delta = b*b -(4**a*c");
-    if(delta>=0)
+    float a,b,c , delta, x1,x2;
+    float sqrtdelta, raiz_1,raiz_2;
+
+    printf("Informe os valores de a, b e c para a equacao f(x) =ax2 +bx+c = 0\n");
+    scanf("%f",&a);
+    scanf("%f",&b);
+    scanf("%f",&c);
+
+    delta = b*b-4*a*c;
+
+    if (delta<0)
     {
-      delta =b*b-4*a*c;
-      if(delta<0)
-      {
-        raiz1=(-b + sqrtdelta)/ (2 * a);
-        raiz2=(-b - sqrtdelta)/ (2 * a);
-        printf( "Raizes :%2f e %2f", raiz 1, raiz2);
-      }
-      else
-      {
-        delta=-delta;
-        sqrtdelta=sqrt(delta);
-        printf("Raiz1:%2f+i. %2\n",(-b)/(2*a),(sqrtdelta)/(2*a));
-        printf("Raiz2:%2f-i. %2f\n",(-b)/(2*a),(sqrtdelta)/(2*a));
-      }
-      , else
-      printf("Coeficiente 'a' inválido, Não é uma eguação do 2ograu");
+        printf("Raizes Imaginarias");
+    } else if(a == 0) {
+        printf("Coeficiente 'a' invalido. Nao eh uma equacao do 2o grau.");
+    } else {
+        sqrtdelta = sqrt(delta);
+        raiz_1 = (-b + sqrtdelta)/ (2 * a);
+        raiz_2 = (-b - sqrtdelta)/ (2 * a);
+        printf( "Raizes: %2f e %2f", raiz_1, raiz_2);
     }
-return 0;
+    return 0;
 }
 
 // b) Dada uma String qualquer inverter essa String.
